cfe0598cc9fb779f3be18e2cece0608fbf2ad380
PedroOSanches <pedrohenriqueoliveirasanches3@gmail.com>
2026-07-28 18:31:42 -0300

    refactor: atualiza UserRepositoryMock para usar UUID em vez de email

--- src/shared/infra/repositories/user_repository_mock.py
+++ src/shared/infra/repositories/user_repository_mock.py
@@ -1,3 +1,4 @@
+import uuid
 from typing import List
 
 from src.shared.domain.entities.user import User
@@ -8,44 +9,41 @@ from src.shared.helpers.errors.usecase_errors import NoItemsFound
 
 class UserRepositoryMock(IUserRepository):
     users: List[User]
-    user_counter: int
 
     def __init__(self):
         self.users = [
             User(email="admin@example.com", role="admin", password_hash="hash_da_senha"),
             User(email="user@example.com", role="user", password_hash="hash_da_senha"),
         ]
-        self.user_counter = 2
 
-    def get_user(self, user_email: str) -> User:
+
+    def get_user(self, user_id: uuid.UUID) -> User:
         for user in self.users:
-            if user.email == user_email:
+            if user.user_id == user_id:
                 return user
-        raise NoItemsFound("user_email")
+        raise NoItemsFound("user_id")
 
     def get_all_user(self) -> List[User]:
         return self.users
 
     def create_user(self, new_user: User) -> User:
         self.users.append(new_user)
-        self.user_counter += 1
         return new_user
 
-    def delete_user(self, user_email: str) -> User:
+    def delete_user(self, user_id: uuid.UUID) -> User:
         for idx, user in enumerate(self.users):
-            if user.email == user_email:
-                self.user_counter -= 1
+            if user.user_id == user_id:
                 return self.users.pop(idx)
 
-        raise NoItemsFound("user_email")
+        raise NoItemsFound("user_id")
 
-    def update_user(self, user_email: str, new_password_hash: str) -> User:
+    def update_user(self, user_id: uuid.UUID, new_password_hash: str) -> User:
         for user in self.users:
-            if user.email == user_email:
+            if user.user_id == user_id:
                 user.password_hash = new_password_hash
                 return user
 
-        raise NoItemsFound("user_email")
+        raise NoItemsFound("user_id")
 
     def get_user_counter(self) -> int:
-        return self.user_counter
+        return len(self.users)
--- tests/shared/infra/repositories/test_user_repository_mock.py
+++ tests/shared/infra/repositories/test_user_repository_mock.py
@@ -1,3 +1,4 @@
+import uuid
 from src.shared.domain.entities.user import User
 from src.shared.domain.enums.role_enum import ROLE
 from src.shared.helpers.errors.usecase_errors import NoItemsFound
@@ -6,54 +7,41 @@ import pytest
 
 
 class Test_UserRepositoryMock:
+
+    def test_create_user(self):
+        repo = UserRepositoryMock()
+        user = User(
+                    email="dohype@vitin.com",
+                    password_hash="hash_da_senha",
+                )
+        repo.create_user(user)
+        assert repo.users[-1].email == "dohype@vitin.com"
+        assert repo.users[-1].password_hash == "hash_da_senha"
+        assert repo.users[-1].role == "user"
+
     def test_get_user(self):
         repo = UserRepositoryMock()
-        user = repo.get_user("admin@example.com")
+        user_id =repo.get_all_user()[1].user_id
+        user = repo.get_user(user_id)
 
-        assert user.email == "admin@example.com"
+        assert user.email == "user@example.com"
         assert user.password_hash == "hash_da_senha"
-        assert user.role == "admin"
+        assert user.role == "user"
 
     def test_get_user_not_found(self):
         repo = UserRepositoryMock()
         with pytest.raises(NoItemsFound):
-            user = repo.get_user("nonexistent@example.com")
+            user = repo.get_user(uuid.uuid4())
 
     def test_get_all_user(self):
         repo = UserRepositoryMock()
         users = repo.get_all_user()
         assert len(users) == 2
 
-    def test_create_user(self):
-        repo = UserRepositoryMock()
-        user = User(
-            email="dohype@vitin.com",
-            password_hash="hash_da_senha",
-        )
-
-        repo.create_user(user)
-
-        assert repo.users[-1].email == "dohype@vitin.com"
-        assert repo.users[-1].password_hash == "hash_da_senha"
-        assert repo.users[-1].role == "user"
-
-        assert repo.user_counter == 3
-
-    def test_delete_user(self):
-        repo = UserRepositoryMock()
-        user = repo.delete_user("user@example.com")
-        assert user.email == "user@example.com"
-        assert user.password_hash == "hash_da_senha"
-        assert user.role == "user"
-
-    def test_delete_user_not_found(self):
-        repo = UserRepositoryMock()
-        with pytest.raises(NoItemsFound):
-            user = repo.delete_user("nonexistent@example.com")
-
     def test_update_user(self):
         repo = UserRepositoryMock()
-        user = repo.update_user("admin@example.com", "new_hash_da_senha")
+        user_id = repo.get_all_user()[0].user_id
+        user = repo.update_user(user_id, "new_hash_da_senha")
 
         assert user.password_hash == "new_hash_da_senha"
         assert repo.users[0].password_hash == "new_hash_da_senha"
@@ -61,10 +49,23 @@ class Test_UserRepositoryMock:
     def test_update_user_not_found(self):
         repo = UserRepositoryMock()
         with pytest.raises(NoItemsFound):
-            user = repo.update_user("nonexistent@example.com", "new_hash_da_senha")
+            user = repo.update_user(uuid.uuid4(), "new_hash_da_senha")
 
     def test_get_users_counter(self):
         repo = UserRepositoryMock()
 
         assert repo.get_user_counter() == 2
 
+    def test_delete_user(self):
+        repo = UserRepositoryMock()
+        user_id = repo.get_all_user()[1].user_id
+        user = repo.delete_user(user_id)
+        assert user.email == "user@example.com"
+        assert user.password_hash == "hash_da_senha"
+        assert user.role == "user"
+        assert repo.get_user_counter() == 1
+
+    def test_delete_user_not_found(self):
+        repo = UserRepositoryMock()
+        with pytest.raises(NoItemsFound):
+            user = repo.delete_user(uuid.uuid4())
