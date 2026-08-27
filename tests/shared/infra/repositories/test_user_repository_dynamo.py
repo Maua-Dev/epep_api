c92352953fad1bd66073e6626f619626fd25b539
PedroOSanches <pedrohenriqueoliveirasanches3@gmail.com>
2026-08-27 20:33:06 -0300

    refat: corrige a formatação dos decorators de teste no Test_UserRepositoryDynamo

--- tests/shared/infra/repositories/test_user_repository_dynamo.py
+++ tests/shared/infra/repositories/test_user_repository_dynamo.py
@@ -7,7 +7,7 @@ from src.shared.infra.repositories.user_repository_mock import UserRepositoryMoc
 
 
 class Test_UserRepositoryDynamo:
-    # @pytest.mark.skip(reason="Needs dynamoDB")
+    @pytest.mark.skip(reason="Needs dynamoDB")
     def test_create_user(self):
         os.environ["STAGE"] = "TEST"
 
@@ -17,7 +17,7 @@ class Test_UserRepositoryDynamo:
 
         assert user_repository_mock.users[0].email == resp.email
 
-    # @pytest.mark.skip(reason="Needs dynamoDB")
+    @pytest.mark.skip(reason="Needs dynamoDB")
     def test_get_user(self):
         os.environ["STAGE"] = "TEST"
 
@@ -27,7 +27,7 @@ class Test_UserRepositoryDynamo:
 
         assert user_repository_mock.users[0].email == resp.email
 
-    # @pytest.mark.skip(reason="Needs dynamoDB")
+    @pytest.mark.skip(reason="Needs dynamoDB")
     def test_get_all_user(self):
         os.environ["STAGE"] = "TEST"
 
@@ -36,7 +36,7 @@ class Test_UserRepositoryDynamo:
         resp = user_repository.get_all_user()
         assert len(user_repository_mock.users) == len(resp)
 
-    # @pytest.mark.skip(reason="Needs dynamoDB") 
+    @pytest.mark.skip(reason="Needs dynamoDB") 
     def test_update_user(self):
         os.environ["STAGE"] = "TEST"
 
@@ -47,7 +47,7 @@ class Test_UserRepositoryDynamo:
 
         assert resp.email == "novo_email@epep.com"
 
-    # @pytest.mark.skip(reason="Needs dynamoDB")
+    @pytest.mark.skip(reason="Needs dynamoDB")
     def test_delete_user(self):
         os.environ["STAGE"] = "TEST"
 
